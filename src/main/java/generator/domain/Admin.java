3b9696ec90b620022618832a49fb3e4bfd9e8997
IceProgramer <2473159069@qq.com>
2023-01-20 17:09:06 +0800

    SpringBoot基础架构

---
+++ src/main/java/generator/domain/Admin.java
@@ -0,0 +1,40 @@
+package generator.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 管理员表
+ * @TableName e_admin
+ */
+@TableName(value ="e_admin")
+@Data
+public class Admin implements Serializable {
+    /**
+     * 用户名
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 账号
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 权限
+     */
+    private Integer role;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/generator/mapper/AdminMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.Admin;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author chenjiahan
+* @description 针对表【e_admin(管理员表)】的数据库操作Mapper
+* @createDate 2023-01-20 17:05:23
+* @Entity generator.domain.Admin
+*/
+public interface AdminMapper extends BaseMapper<Admin> {
+
+}
+
+
+
+
